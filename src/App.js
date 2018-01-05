ea0b983e3fdbcc98469e57b841d46274fd58014b
vile- <vilde70@gmail.com>
2018-01-05 02:14:13 +0200

    Filtering works!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import restaurantJson from './restaurants.json'
+const axios = require("axios");
+const Data = require( "./data2.js" );
+
 
 // const data = new Data();
 // data.getRestaurants(); //lista
@@ -9,18 +12,43 @@ import restaurantJson from './restaurants.json'
 // data.filterByCategory( ["G", "+", "f" ]);
 // data.filterByStringAndCategory( "str", []);
 
+function loadData(){
+    return axios.get( "https://skinfo.dy.fi/api/restaurants.json" )
+    .then( response => {
+        return response.data;
+    });
+}
+
 class Restaurant extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {restaurant: props.restaurant}
+    // this.state = {restaurant: props.restaurant}
   }
 
   render() {
-
+    const restaurant = this.props.restaurant;
+    // console.log(restaurant);
+    if (restaurant === undefined){
+      return <div></div>;
+    }
+    let foods = restaurant.foods;
+    // console.log(foods);
+    if (foods !== undefined && foods.length > 0){
+      // console.log(foods);
+      foods  = foods.map((food, index) =>
+        <div key={index}>
+          <div>{food.category}</div>
+          <div>{food.name}</div>
+        </div>
+      );
+    } else {
+      foods = <div>Ei ruokaa</div>
+    }
+    // console.log(restaurant);
     return (
-      const restaurant = this.props.restaurant;
       <div className="restaurant">
-        <h2>{restaurant.name}
+        <h2>{restaurant.name}</h2>
+        {foods}
       </div>
     )
   }
@@ -29,24 +57,25 @@ class Restaurant extends React.Component {
 class RestaurantData extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { search: "",
-                   filters: []}
+    // this.state = { search: "",
+    //                filters: []}
   }
 
   render() {
     if (!this.props.restaurants) {
-      return "Ei ravintoloita"
+      return ""
     }
     const restaurants = this.props.restaurants;
-    console.log(restaurants);
+    // console.log(restaurants);
     // for(let restaurant in restaurants) {
     //   console.log(restaurant.days);
     // }
-    // const restaurantList = restaurants.map((restaurant) =>
-    //   <Restaurant restaurant={restaurant}/>
-    // );
-    // return <div>{restaurantList}</div>
-    return <div></div>
+    const restaurantList = restaurants.map((restaurant, index) =>
+      <Restaurant key={index} restaurant={restaurant}/>
+    );
+    // console.log(restaurantList);
+    return <div>{restaurantList}</div>
+    // return <div></div>
 
   }
 }
@@ -54,11 +83,43 @@ class RestaurantData extends React.Component {
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {restaurants: null}
+    this.state = {data: null,
+                  restaurants: null,
+                  search: "",
+                  filters: []}
+    this.filter = this.filter.bind(this)
   }
 
   componentDidMount(){
-    this.setState({restaurants:restaurantJson})
+    // this.setState({restaurants:restaurantJson})
+    let data = null;
+    loadData().then( res => {
+        data = new Data( res );
+        let restaurants = data.getRestaurants();
+        this.setState({data:data, restaurants:restaurants})
+    })
+  }
+  filter(evt) {
+    const val = evt.target.value;
+    let filters = this.state.filters;
+    let search = this.state.search;
+    if (evt.target.type == "checkbox"){
+      const index = filters.indexOf(val);
+      if (index > -1){
+        filters.splice(index, 1)
+      } else{
+        filters.push(val)
+      }
+    }
+
+    if (evt.target.type == "text"){
+      search = val;
+    }
+    const restaurants = this.state.data.filterRestaurants( 0, search, filters);
+    this.setState({filters:filters, search:search, restaurants:restaurants});
+    console.log(0, search, filters);
+    console.log(this.state.data.getRestaurants());
+    // console.log(restaurants);
   }
 
   render() {
@@ -68,6 +129,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <input type="text" value={this.state.value} onChange={this.filter}></input>
+        <input type="checkbox" value="g" onChange={this.filter}></input>
+        <input type="checkbox" value="m" onChange={this.filter}></input>
+        <input type="checkbox" value="+" onChange={this.filter}></input>
         <RestaurantData restaurants={this.state.restaurants}/>
       </div>
     );
---
+++ src/data2.js
@@ -0,0 +1,89 @@
+const axios = require("axios");
+
+module.exports = class Data{
+	constructor( data ){
+		this.categories = [ "ylioppilastalo", "amk", "laseri", "indiana-curry", "cafe-bar-g", "lut-buffet", "ravintola-parempi" ];
+		this.restaurants = [];
+		//let index = 0;
+		this.categories.forEach( element => {
+			//Single restaurant from API.
+			let e = data[ element ];
+			if( e.days !== null ){
+				//For supporting many days
+				//this.restaurants.push( [] );
+				let days = Object.getOwnPropertyNames( e.days );
+
+				let restaurant = null;
+				//For multiple days, return at the bottom keeps it for one-day only for this prototype
+				days.forEach( d => {
+					let day = e.days[ d ];
+					restaurant = {};
+					restaurant.name = e.name;
+					restaurant.link = e.link;
+					restaurant.hasStudentDiscounts = e.student_discounts;
+					restaurant.date = d;
+					restaurant.startTime = day.lunch_times.start;
+					restaurant.endTime = day.lunch_times.end;
+
+					restaurant.foods = [];
+					day.foods.forEach( food => {
+						let f = {};
+						f.name = food.title_fi;
+						f.description = food.description;
+						f.diet = food.diet;
+						f.category = food.category;
+						restaurant.foods.push( f );
+					})
+					//Just todays data "break"
+					return;
+				});
+				if( restaurant != null )
+					this.restaurants.push( restaurant );
+					//For supporting many days
+					//this.restaurants[ index++ ].push( restaurant );
+			}
+		})
+	}
+
+	//Returns array of all restaurants.
+	getRestaurants(){
+		return this.restaurants;
+	}
+
+	//Returns filtered array of restaurants. Day is not yet implemented.
+	//filterStr filters food name by users input
+	//filterCategories filter by things like diets.
+	filterRestaurants( day, filterStr = "", filterCategories = null ){
+		if( filterStr !== "" || filterCategories !== null ){
+
+			// const filtered = this.restaurants.slice();
+			const filtered = JSON.parse(JSON.stringify(this.restaurants))
+			filtered.forEach( restaurant => {
+				restaurant.foods = restaurant.foods.filter( food => {
+					//Indicates if particular food fits in filter.
+					let fitting = 1;
+					if( filterStr !== "" ){
+						fitting = food.name.toLowerCase().includes( filterStr.toLowerCase()) ? 1 : 0;
+					}
+
+					if( filterCategories !== null && fitting !== 0 ){
+						filterCategories.forEach( filter => {
+							fitting = food.diet.toLowerCase().includes( filter.toLowerCase()) ? 1 : 0;
+							if( fitting === 0 )
+								return;
+						})
+					}
+					return fitting;
+				})
+			})
+			//Moves restaurants with empty lists to the end
+			filtered.sort( ( a, b ) => {
+				if( a.foods.length === 0 )
+					return 1
+				return 0
+			})
+			return filtered;
+		}
+		return this.restaurants;
+	}
+}
